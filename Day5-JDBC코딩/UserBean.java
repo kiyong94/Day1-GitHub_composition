29e2386ac4698b6e91cb55851c9e577593b73345
kiyong94 <117061516+kiyong94@users.noreply.github.com>
2022-12-10 02:47:14 +0900

    Create UserBean.java
    CSVSelect 파일을 위한 bean 클래스

---
+++ Day5-JDBC코딩/UserBean.java
@@ -0,0 +1,40 @@
+package day5_lib;
+
+public class UserBean {
+	String name;
+	String position;
+	String height;
+	String weight;
+	String age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+}
